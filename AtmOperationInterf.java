14a9a4c6cce7f9e4d64f8caa4c2b7f7674424500
Suvishreddy <94463338+Suvishreddy@users.noreply.github.com>
2025-02-12 15:32:10 +0530

    Create AtmOperationInterf.java

---
+++ AtmOperationInterf.java
@@ -0,0 +1,8 @@
+package sai;
+// created an interface 
+public interface AtmOperationInterf {
+	public void viewBalance();
+	public void withdrawAmount(double withdrawAmount);
+	public void depositAmount(double depositAmount);
+	public void viewMiniStatement();
+}
